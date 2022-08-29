752a7691e667b48fd22995d01d2d1b73fb5611f5
Simon Ullerstam <ens19sum@cs.umu.se>
2022-08-29 14:16:47 +0200

    some dev

--- src/Grammar.java
+++ src/Grammar.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public abstract class Grammar {
-    private HashMap<Character, Integer> nonTerminals;
-    private ArrayList<ArrayList<String>> rules;
+    private final HashMap<Character, Integer> nonTerminals;
+    private final ArrayList<ArrayList<String>> rules;
 
     public Grammar() {
         nonTerminals = new HashMap<>();
         rules = new ArrayList<>();
     }
 
-    public abstract void readGrammar(String[] rules);
+    public abstract void read(String[] rules);
+
+    public abstract void read(String fileName);
 
     public HashMap<Character, Integer> getNonTerminals() {
         return nonTerminals;
@@ -18,5 +21,13 @@ public abstract class Grammar {
 
     public ArrayList<ArrayList<String>> getRules() {
         return rules;
+    }
+
+    public ArrayList<String> getRule(char nonTerminal) {
+        try { // Ksk ändra till if-sats ???
+            return rules.get(nonTerminals.get(nonTerminal));
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,16 +11,17 @@ public class Main {
 
         System.out.println(g.getNonTerminals());
         System.out.println(g.getRules());
+        System.out.println(g.getRule('X'));
         Parser.parseNaive(g, "ab");
     }
 
     private static Grammar createGrammarFromStringArr(String[] rules) {
         Grammar g = new Grammar() {
             @Override
-            public void readGrammar(String[] input) {
+            public void read(String[] input) {
                 HashMap<Character, Integer> nonTerminals = getNonTerminals();
                 ArrayList<ArrayList<String>> rules = getRules();
-                int counter = 0;
+                int counter = 0; // Replace counter with nonTerminals.length()-1 ???
                 for (String rule : input) {
                     char nonTerminal = rule.charAt(0);
                     if (!nonTerminals.containsKey(nonTerminal)) {
@@ -31,8 +33,37 @@ public class Main {
                     rules.get(index).add(result);
                 }
             }
+
+            @Override
+            public void read(String fileName) {
+                System.out.println("ERROR: Wrong read method called");
+            }
         };
-        g.readGrammar(rules);
+        g.read(rules);
+        return g;
+    }
+
+    private static Grammar createGrammarFromFile(String fileName) { // NOT implemented !!!
+        Grammar g = new Grammar() {
+            @Override
+            public void read(String[] rules) {
+                System.out.println("ERROR: Wrong read method called");
+            }
+
+            @Override
+            public void read(String fileName) {
+                try {
+                    InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName);
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                    System.exit(1);
+                }
+                HashMap<Character, Integer> nonTerminals = getNonTerminals();
+                ArrayList<ArrayList<String>> rules = getRules();
+
+            }
+        };
+        g.read(fileName);
         return g;
     }
 }
