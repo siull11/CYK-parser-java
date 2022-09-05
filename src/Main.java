dc3707474008b0f59a2f3feb021427dbb84978f5
Simon Ullerstam <ens19sum@cs.umu.se>
2022-09-05 11:24:37 +0200

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start of program");
-        //Grammar g = new GrammarDyck();
-        Grammar g = new GrammarStupid();
+        Grammar g = new GrammarDyck();
+//        Grammar g = new GrammarStupid();
         Parser p = new ParserBU();
-        System.out.println("Ans: " + p.parse(g, "((())(()))") + ", Counter: " + p.getCounter());
+        System.out.println("Ans: " + p.parse(g, "((())(()))") + ", c: " + p.getCounter());
+//        System.out.println("Ans: " + p.parse(g, "aaaaa") + ", c: " + p.getCounter());
     }
 }
--- src/ParserBU.java
+++ src/ParserBU.java
@@ -3,13 +3,14 @@ public class ParserBU implements Parser {
 
     private int counter;
 
+    // Skapa v2 av denna med int arr ist f bool & ksk v3 med HashSet !!!
     @Override
     public boolean parse(Grammar g, String s) {
-        int len = s.length();
-        boolean[][][] res = new boolean[len][len][g.numNT];
+        int n = s.length();
+        boolean[][][] res = new boolean[n][n][g.numNT];
 
-        // Convert terminals to non-terminals
-        for (int i = 0; i < len; i++) {
+        // Convert terminals to non-terminals (fill bottom row)
+        for (int i = 0; i < n; i++) {
             char t = s.charAt(i);
             if (!g.T_to_NT.containsKey(t)) {
                 return false;
@@ -18,32 +19,34 @@ public class ParserBU implements Parser {
         }
 
         // Fill rest of the table
-        for (int y = 1; y < len; y++) { // loop over rows
-            for (int x = 0; x < len-y; x++) { // loop over columns
+        for (int y = 1; y < n; y++) { // loop over rows
+            for (int x = 0; x < n-y; x++) { // loop over columns
                 boolean[] newCell = new boolean[g.numNT];
+
                 for (int i = 0; i < y; i++) { // loop over child cells
                     boolean[] under = res[x][i];
                     boolean[] diagonal = res[x+i+1][y-i-1];
-                    // loop over non-terminals
-                    for (int u = 0; u < g.numNT; u++) {
-                        for (int d = 0; d < g.numNT; d++) {
-                            if (under[u] && diagonal[d]) {
-                                int[] nts = g.NTs_to_NT[u][d];
-                                if (nts == null) {
-                                    continue;
-                                }
-                                for (int n: nts) {
-                                    newCell[n] = true;
-                                }
+
+                    for (int u = 0; u < g.numNT; u++) { // loop over non-terminals under
+                        if (!under[u]) continue;
+
+                        for (int d = 0; d < g.numNT; d++) { // loop over non-terminals diagonal
+                            if (!diagonal[d]) continue;
+
+                            int[] nts = g.NTs_to_NT[u][d]; // get non-terminals that produce u and d
+                            if (nts == null) continue;
+
+                            for (int nt: nts) {
+                                newCell[nt] = true;
+                                counter++; // Flytta ???
                             }
-                            counter++; // Flytta innåt???
                         }
                     }
                 }
                 res[x][y] = newCell;
             }
         }
-        return res[0][len-1][g.start];
+        return res[0][n-1][g.start];
     }
 
     @Override
